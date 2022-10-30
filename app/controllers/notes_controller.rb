d9df7815ff5edbaafbc4d5c9fce370032d01c8bc
Denis Vakar <nuclbain@gmail.com>
2022-10-30 08:37:25 +0200

    Added controllers for basic models

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,77 @@
+# frozen_string_literal: true
+
+# This is the notes controller, which is responsible for handling requests for notes.
+class NotesController < ApplicationController
+  before_action :set_board
+  before_action :set_note, only: %i[show edit update destroy]
+  before_action :authenticate_user!
+
+  # GET /notes or /notes.json
+  def index
+    @notes = @board.notes
+  end
+
+  # GET /notes/1 or /notes/1.json
+  def show; end
+
+  # GET /notes/new
+  def new
+    @note = @board.notes.new
+  end
+
+  # GET /notes/1/edit
+  def edit; end
+
+  # POST /notes or /notes.json
+  def create
+    @note = @board.notes.new(note_params)
+
+    respond_to do |format|
+      if @note.save
+        format.html { redirect_to [@board, @note], notice: 'Note was successfully created.' }
+        format.json { render :show, status: :created, location: @note }
+      else
+        format.html { render :new, status: :unprocessable_entity }
+        format.json { render json: @note.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /notes/1 or /notes/1.json
+  def update
+    respond_to do |format|
+      if @note.update(note_params)
+        format.html { redirect_to [@board, @note], notice: 'Note was successfully updated.' }
+        format.json { render :show, status: :ok, location: @note }
+      else
+        format.html { render :edit, status: :unprocessable_entity }
+        format.json { render json: @note.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /notes/1 or /notes/1.json
+  def destroy
+    @note.destroy
+    respond_to do |format|
+      format.html { redirect_to board_notes_url(@board), notice: 'Note was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_board
+    @board = Board.find(params[:board_id])
+  end
+
+  def set_note
+    @note = @board.notes.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def note_params
+    params.require(:note).permit(:title, :description, :content)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,11 @@
 Rails.application.routes.draw do
   devise_for :users
 
+  resources :users
+
+  resources :boards do
+    resources :notes
+  end
+
   root 'static_pages#home'
 end
