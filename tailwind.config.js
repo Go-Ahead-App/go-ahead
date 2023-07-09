a3493ebb7508ee52bbba4595010ad2cef7469e8b
Denis Vakar <nuclbain@gmail.com>
2023-07-09 20:50:31 +0300

    Revised landing design and site identity

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,17 @@ module.exports = {
   theme: {
     fontFamily: {
       'display': ["Miratrix", "sans-serif"],
+      'title': ["Syne", "sans-serif"],
     },
+    extend: {
+      fontSize: {
+        '10xl': '10rem',
+        '11xl': '11rem',
+        '12xl': '12rem',
+        '13xl': '13rem',
+        '14xl': '14rem',
+        '15xl': '15rem',
+      }
+    }
   },
 }
